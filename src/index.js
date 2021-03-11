cc10905baf77acfa2b4f2fa49b1d90da5a859147
zone0119 <54246376+zone0119@users.noreply.github.com>
2021-03-11 12:52:44 +0600

    I write code for min function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-	console.log(min);
+    
+  //console.log(arguments);
+    
+    var minCurr;
+    for (let index = 0; index <= arguments.length; index++) 
+    {
+        
+            if(index == 0)
+            {
+                minCurr = arguments[index];
+            }
+            else if(arguments[index] < minCurr)
+            {   
+                minCurr = arguments[index];
+            }
+
+        
+    }
+    //console.log("minCurr : " + minCurr);
 	
-	
-		console.log(array);
-	
-	
-  return 0;
+  return minCurr;
 }
 
 exports.max = function max (array) {
